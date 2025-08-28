eaccba91ca4bef01f7c8a3720ba1f06dfea55387
Hussain Basha <hussainbasha1274@gmail.com>
2025-08-28 09:53:24 -0500

    Intial parent project setup

---
+++ autoservice360/autoservice360/src/main/java/com/autoservice/autoservice360/Autoservice360Application.java
@@ -0,0 +1,13 @@
+package com.autoservice.autoservice360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Autoservice360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Autoservice360Application.class, args);
+	}
+
+}
